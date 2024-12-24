49c9f79c98ae0d7bbc11ae06e4162970020a423f
HaHAjax <128398800+HaHAjax@users.noreply.github.com>
2024-12-23 18:42:31 -0700

    Debug UI in GDScript

---
+++ Scenes/Test/DebugUI_Camera-Player.gd
@@ -0,0 +1,44 @@
+extends CanvasLayer
+
+var playerControllerScript: CharacterBody3D
+
+var labelPaths: Array[String] = [
+	"DebugVariablesLeft/currentState/CurrStateDebugValue",
+	"DebugVariablesLeft/IsPlayerRolling/CanRollDebugValue",
+	"DebugVariablesLeft/MoveDirection/MoveDirDebugValue",
+	"DebugVariablesLeft/LookDirection/LookDirDebugValue",
+	"DebugVariablesLeft/RollDirection/RollDirDebugValue",
+	"DebugVariablesLeft/InputDoRoll/IDoRollDebugValue",
+	"DebugVariablesLeft/InputMoveDirection/IMoveDirDebugValue",
+	"DebugVariablesLeft/InputRollDirection/IRollDirDebugValue",
+	"DebugVariablesLeft/RollTimer/RollTimeDebugValue",
+	"DebugVariablesLeft/RollCooldown/RollCoolDebugValue"
+]
+
+var variableNames: Array[String] = [
+	"curr_state",
+	"isActivelyRolling",
+	"moveDirection",
+	"lookDirection",
+	"rollDirection",
+	"inputDoRollButton",
+	"inputMoveDirection",
+	"inputRollDirection",
+	"timerRoll",
+	"timerRollCooldown"
+]
+
+func _ready():
+	playerControllerScript = $"../Player"
+
+func _physics_process(delta):
+	for i in range(labelPaths.size()):
+		var labelPath = labelPaths[i]
+		var label: Label = get_node(labelPath)
+		var varName = variableNames[i]
+		
+		# Using reflection to get the value of the variable from playerControllerScript
+		var varValue = playerControllerScript.get(varName)
+		
+		# Setting the label's text to the variable's value
+		label.text = str(varValue)
